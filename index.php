50838e18d491d72d8125a55b3ecb39c489d2ea94
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-07-05 16:55:47 +0200

    add string data in server and main.js

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!-- Descrizione
+Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.
+Stack
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
+Consigli
+Nello svolgere l’esercizio seguite un approccio graduale.
+Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”).
+Solo a questo punto sarà utile passare alla lettura della lista da un file JSON. -->
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +20,9 @@
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
-    
+    <div id="app">
+
+    </div>
 
 
 
---
+++ server.php
@@ -0,0 +1,6 @@
+<?php 
+
+$rawData = file_get_contents('./database/dischi.json');
+
+var_dump($rawData);
+?>
